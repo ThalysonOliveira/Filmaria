3a79b0e5aa1edd076a08551ab6e56191f8a8e861
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-04 22:50:18 -0200

    Criado metodo componentDidmount e importado link

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     constructor(props) {
@@ -7,10 +8,28 @@ export default class Home extends Component {
             filmes: []
         }
     }
+
+    async componentDidMount() {
+        const url = 'https://sujeitoprogramador.com/r-api/?api=filmes/'
+        const response = await fetch(url)
+        const filme = await response.json()
+        this.setState({ filmes: filme })
+    }
+
     render() {
         return (
             <div>
-
+                <div>
+                    {this.state.filmes.map((filme) => {
+                        return (
+                            <article key={filme.id} className="filme">
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} alt="capa" />
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
