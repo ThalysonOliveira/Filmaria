40e723bf07059730fb3f62d118527186062f30b6
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-04 22:28:20 -0200

    Criado rota para Header

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Header from './components/Header'
+
+const Routes = ()=>{
+    <BrowserRouter>
+        <Header/>
+        <Switch>
+
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Routes
+
+
