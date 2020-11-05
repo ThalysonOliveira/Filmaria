051a78e8c241b43f7347ce8b58f27eb5e08b0b3b
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-04 22:43:22 -0200

    Adiconado rotas: home, filme e erro

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import Header from './components/Header'
+
+import Home from './pages/Home'
+import Filme from './pages/Filme'
+import Erro from './pages/Erro'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Header />
             <Switch>
-
+                <Route exact path="/" component={Home} />
+                <Route exat path="/filme/:id" component={Filme} />
+                <Route path="*" component={Erro} />
             </Switch>
         </BrowserRouter>
     )
