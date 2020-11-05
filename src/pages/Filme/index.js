50602cc6b2a5f0989826a4708a2a8ff744b65eb1
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-04 23:13:13 -0200

    Criado metodo componentDidMount

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 
 
 export default class Filme extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      
+    this.state = {
+      filme: []
     }
   }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params
+    const url = `https://sujeitoprogramador.com/r-api/?api=filmes/${id}`
+    const reponse = await fetch(url)
+    const f = await reponse.json()
+    this.setState({filme: f})
+  }
+
   render() {
     return (
       <div>
-          
+
       </div>
     )
   }
